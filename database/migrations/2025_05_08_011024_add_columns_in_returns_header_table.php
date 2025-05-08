81077b362c91b7888b0ab00bd574b367ea095802
CrisLloyd2030 <crislloyddalina@digits.ph>
2025-05-08 13:21:08 +0800

    Update: address migration

---
+++ database/migrations/2025_05_08_011024_add_columns_in_returns_header_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsInReturnsHeaderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('returns_header', function (Blueprint $table) {
+            $table->string('country')->nullable();
+            $table->string('province')->nullable();
+            $table->string('city')->nullable();
+            $table->string('barangay')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('returns_header', function (Blueprint $table) {
+
+            $table->dropColumn('country');
+            $table->dropColumn('province');
+            $table->dropColumn('city');
+            $table->dropColumn('barangay');
+        });
+    }
+}
