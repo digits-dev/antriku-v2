ade29936493d9bd1769107b9f6557cef30d22c6e
CrisLloyd2030 <crislloyddalina@digits.ph>
2025-06-24 17:50:26 +0800

    Update API Credintials

--- config/api.php
+++ config/api.php
@@ -2,7 +2,7 @@
 
 return [
     'dimfs_secret_key' => '4fea7e3c217fa58b26d57fc186e906b4',
-    'dimfs_api_created_link' => 'https://dimfs.digitstrading.ph/public/api/tickects_btb_created',
-    'dimfs_parts_item_api_created_link' => 'https://dimfs.digitstrading.ph/public/api/tickects_btb_parts_item_created',
+    'dimfs_api_created_link' => 'https://dimfs.digitstrading.ph/api/tickects_btb_created',
+    'dimfs_parts_item_api_created_link' => 'https://dimfs.digitstrading.ph/api/tickects_btb_parts_item_created',
     'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
 ];
